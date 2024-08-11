054f1ff9b3e03a12f79973337df254440ba06fd2
Zeege <992793626@qq.com>
2024-08-11 21:51:26 +0800

    add json_to_csv

--- test/test_timer.py
+++ test/test_timer.py
@@ -2,8 +2,18 @@ import time
 import torch
 import pandas as pd
 from globaler import Timer
+from globaler.timer import TimeRecord
 import torch
 import torch.nn.functional as F
+import os
+
+
+
+csv = Timer.json_to_csv(open("/home/lzj/work/llm-infer/AMotivation/results/timer/2024-08-11_21:29:46.json").read())
+with open("/home/lzj/work/llm-infer/AMotivation/results/timer/2024-08-11_21:29:46.csv", "w+") as f:
+    f.write(pd.DataFrame(csv).to_csv())
+
+exit(1)
 
 
 X = torch.randn((4096, 5120), device='cuda')
