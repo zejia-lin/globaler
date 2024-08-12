c07dc0c49394f0202712c407723ad95536c38b97
Zeege <992793626@qq.com>
2024-08-12 21:03:30 +0800

    add bypass for Enabled and DebugOnly

--- globaler/enabler.py
+++ globaler/enabler.py
@@ -23,24 +23,35 @@ class _EnablerMetaClass(type):
             # No-op if not enabled
             return None
         return wrapper
+    
+    @staticmethod
+    def _override(func, keyword):
+        setattr(func, keyword, True)
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            return func(*args, **kwargs)
+        return wrapper
 
     def __new__(cls, name, bases, dct):
+        # Checking base class for Enabled and DebugOnly
         if len(bases) > 0:
             for c in bases:
                 if issubclass(c, (DebugOnly, Enabled)):
-                    enabler_base_cls = c
+                    cls.enabled = c.enabled
+                    cls.debug_only = c.debug_only
                     break
-            cls.enabled = enabler_base_cls.enabled
-            cls.debug_only = enabler_base_cls.debug_only
-        else: 
+        else:
             cls.enabled = dct.get("enabled")
             cls.debug_only = dct.get("debug_only")
+
+        # Decorating all methods with enabler and debug_only
         for attr_name, attr_value in dct.items():
             if isinstance(attr_value, types.FunctionType) and not (
                 attr_name.startswith("__") and attr_name.endswith("__")
             ):
-                dct[attr_name] = _EnablerMetaClass._enabler(attr_value)
-                if cls.debug_only:
+                if not hasattr(attr_value, "_override_enabled"):
+                    dct[attr_name] = _EnablerMetaClass._enabler(attr_value)
+                if cls.debug_only and not hasattr(attr_value, "_override_debug_only"):
                     dct[attr_name] = debug_only(dct[attr_name])
         return super().__new__(cls, name, bases, dct)
 
@@ -52,6 +63,10 @@ class DebugOnly(metaclass=_EnablerMetaClass):
         super().__init_subclass__(**kwargs)
         cls.enabled = True
         cls.debug_only = True
+    
+    @staticmethod
+    def bypass(func):
+        return _EnablerMetaClass._override(func, "_override_debug_only")
 
 class Enabled(metaclass=_EnablerMetaClass):
     enabled = True
@@ -60,3 +75,7 @@ class Enabled(metaclass=_EnablerMetaClass):
         super().__init_subclass__(**kwargs)
         cls.enabled = True
         cls.debug_only = False
+
+    @staticmethod
+    def bypass(func):
+        return _EnablerMetaClass._override(func, "_override_enabled")
--- test/test_enabler.py
+++ test/test_enabler.py
@@ -1,23 +1,35 @@
 import time
-from globaler.enabler import DebugOnly
+from globaler.enabler import DebugOnly, Enabled
+from functools import wraps
+
+def dec(func):
+    setattr(func, "_override_enabler", True)
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        return func(*args, **kwargs)
+    return wrapper
 
 # Example usage
 class A(DebugOnly):
+    @DebugOnly.bypass
     def method(self, a):
         print(a)
         return len(a) * 3.14159
+    
+    @dec
+    def m2(self):
+        pass
 
 
 a = A()
 b = A()
-# a.enabled = False
-a.method("Hello")  # Prints "Hello" because enabled is True
+
+a.method("Hello")
 
 a.enabled = False
-a.method("Hello 2")  # Does nothing because enabled is False
-# b.enabled = True
+a.method("Hello 2")
+
 b.method("Hello 3")
-# print(A.enabled, a.enabled, b.enabled)
 
 # start_time = time.time()
 # for _ in range(1000000):
